d8ae7a089e5b7d215e94e8e726dc1dc9da26327e
MakiaV <bertrand.youago@gmail.com>
2023-10-01 23:47:56 +0200

    initial commit

---
+++ components/Header.tsx
@@ -0,0 +1,34 @@
+import Image from "next/image";
+import logo from "../public/images/logo.png";
+import { LiaOpencart } from "react-icons/lia";
+import { BiUser } from "react-icons/bi";
+
+const Header = () => {
+	return (
+		<div className="fixed top-0 left-0 w-full border border-b-1">
+			<div className="flex px-[150px] py-4  justify-between items-center w-full z-50 bg-white">
+				<Image src={logo} alt="img" width={50} height={50} />
+				<div className="flex gap-x-8">
+					<p className="cursor-pointer font-semibold hover:text-green-800">
+						MEN
+					</p>
+					<p className="cursor-pointer font-semibold hover:text-green-800">
+						WOMEN
+					</p>
+					<p className="cursor-pointer font-semibold hover:text-green-800">
+						KIDS
+					</p>
+					<p className="cursor-pointer font-semibold hover:text-green-800">
+						COLLECTION
+					</p>
+				</div>
+				<div className="flex items-center gap-x-4">
+					<BiUser size={22} />
+					<LiaOpencart size={25} />
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Header;
---
+++ components/Product.tsx
@@ -0,0 +1,125 @@
+import Image from "next/image";
+import lemon from "../public/images/lemon.avif";
+import white from "../public/images/white.avif";
+import black from "../public/images/black.avif";
+import { LiaOpencart } from "react-icons/lia";
+import { useLayoutEffect, useRef } from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+const Product = () => {
+	const productImage = useRef(null);
+	const productDetails = useRef(null);
+	const addToCart = useRef(null);
+	const myBox = useRef(null);
+
+	useLayoutEffect(() => {
+		gsap.registerPlugin(ScrollTrigger);
+		gsap.to(productImage.current, {
+			scrollTrigger: {
+				trigger: productImage.current,
+				start: "top 60px",
+
+				scrub: true,
+			},
+			scale: 0.5,
+			transformOrigin: "0 0",
+			duration: 1,
+		});
+		gsap.to(productDetails.current, {
+			scrollTrigger: {
+				trigger: productDetails.current,
+				start: "top 60px",
+
+				scrub: true,
+			},
+			y: 50,
+			ease: "power1.in",
+			opacity: 0,
+
+			duration: 0.2,
+		});
+		gsap.to(addToCart.current, {
+			scrollTrigger: {
+				trigger: addToCart.current,
+				start: "top 60px",
+
+				scrub: true,
+			},
+			y: -150,
+			ease: "power1.in",
+
+			duration: 0.2,
+		});
+		gsap.to(myBox.current, {
+			scrollTrigger: {
+				trigger: myBox.current,
+				start: "top 60px",
+				scrub: true,
+			},
+			height: "150px",
+			paddingBottom: "50px",
+			ease: "power1.in",
+
+			duration: 0.2,
+		});
+	}, []);
+	return (
+		<div
+			className="mt-[68px] 2xl:px-[200px] z-50 fixed w-full px-[150px] bg-white mb-[30px]"
+			ref={myBox}
+		>
+			<div className="flex gap-x-4  h-auto">
+				<Image
+					src={lemon}
+					alt="img"
+					width={300}
+					height={300}
+					ref={productImage}
+					className=""
+				/>
+				<div
+					className="flex flex-col justify-between flex-grow"
+					ref={addToCart}
+				>
+					<div ref={productDetails}>
+						<p>Football</p>
+						<p className="text-3xl font-bold">X CRAZYFAST.1 FG</p>
+						<p className="font-semibold">€ 260</p>
+					</div>
+					<div className="flex  items-end justify-between ">
+						<button className="flex border-2 h-[50px] items-center w-[300px] justify-center gap-x-12">
+							<p>ADD TO CART</p>
+							<LiaOpencart size={25} />
+						</button>
+						<div className="">
+							<p>3 colors available</p>
+							<div className="flex gap-x-4">
+								<Image
+									src={black}
+									alt="img"
+									width={80}
+									height={80}
+								/>
+								<Image
+									src={white}
+									alt="img"
+									width={80}
+									height={80}
+								/>
+								<Image
+									src={lemon}
+									alt="img"
+									width={80}
+									height={80}
+								/>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Product;
