02bb580cadc31011da13602364b6813ffe93569b
MakiaV <bertrand.youago@gmail.com>
2023-10-02 08:21:26 +0200

    fix deleted branch 3

--- app/page.tsx
+++ app/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
 			<Header />
 
 			<Product />
-			<div className="py-[400px] px-[150px]  2xl:px-[200px]">
+			<div className="py-[400px] px-[150px]  2xl:px-[250px]">
 				<ProductDetails />
 			</div>
 		</div>
--- components/Header.tsx
+++ components/Header.tsx
@@ -6,7 +6,7 @@ import { BiUser } from "react-icons/bi";
 const Header = () => {
 	return (
 		<div className="fixed top-0 left-0 w-full border border-b-1">
-			<div className="flex px-[150px]  2xl:px-[200px] py-4  justify-between items-center w-full z-50 bg-white">
+			<div className="flex px-[150px]  2xl:px-[250px] py-4  justify-between items-center w-full z-50 bg-white">
 				<Image src={logo} alt="img" width={50} height={50} />
 				<div className="flex gap-x-8">
 					<p className="cursor-pointer font-semibold hover:text-green-800">
